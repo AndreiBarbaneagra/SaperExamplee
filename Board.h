f2e861bae0936361c4d30308bc92ebb16cce6037
AndreiBarbaneagra <andreibarbaneagra1@gmail.com>
2023-11-13 10:10:27 +0200

    Added a description of methods, classes and meta information, and also filled in the file README.md

--- Board.h
+++ Board.h
@@ -1,22 +1,77 @@
 #pragma once
 
+/**
+ * @file Board.h
+ *
+ * @brief Project: Minesweeper
+ * @author Barbaneagra Andrei
+ * @date 2023
+ *
+ * @class Board
+ *
+ * @brief Represents the game board for Minesweeper.
+ */
+
 #include <vector>
 #include "cell.h"
 
 class Board {
 private:
-    int width;
-    int height;
-    std::vector<std::vector<Cell>> cells;
+    int width; /**< Width of the game board. */
+    int height; /**< Height of the game board. */
+    std::vector<std::vector<Cell>> cells; /**< 2D vector representing the cells on the board. */
 
 public:
+
+    /**
+     * @brief Constructor for the Board class.
+     * @param w Width of the board.
+     * @param h Height of the board.
+     */
+
     Board(int w, int h);
+
+    /**
+     * @brief Destructor for the Board class.
+     */
     ~Board();
+
+    /**
+     * @brief Generates mines on the board.
+     * @param numMines Number of mines to generate.
+     */
     void generateMines(int numMines);
+
+    /**
+     * @brief Prints the current state of the board.
+     */
     void printBoard();
+
+    /**
+     * @brief Reveals a cell on the board.
+     * @param x X-coordinate of the cell.
+     * @param y Y-coordinate of the cell.
+     */
     void revealCell(int x, int y);
+
+    /**
+     * @brief Reveals a cell on the board and checks if the player has lost.
+     * @param x X-coordinate of the cell.
+     * @param y Y-coordinate of the cell.
+     */
     void revealCellByUser(int x, int y);
+
+    /**
+     * @brief Checks if the player has lost the game.
+     * @return True if the player has lost, false otherwise.
+     */
     bool hasPlayerLost();
+
+    /**
+     * @brief Overloaded equality operator for Board class.
+     * @param other Another Board object to compare.
+     * @return True if the boards are equal, false otherwise.
+     */
     bool operator==(const Board& other) const;
 
 };
--- Cell.h
+++ Cell.h
@@ -1,14 +1,48 @@
 #pragma once
 
+/**
+ * @file Cell.h
+ *
+ * @brief Project: Minesweeper
+ * @author Barbaneagra Andrei
+ * @date 2023
+ *
+ * @class Cell
+ *
+ * @brief Represents an individual cell on the Minesweeper board.
+ */
+
 class Cell {
 private:
-	bool isMine;
-	bool isRevealed;
+	bool isMine; /**< Indicates whether the cell contains a mine. */
+	bool isRevealed; /**< Indicates whether the cell has been revealed. */
 
 public:
+
+	/**
+	 * @brief Constructor for the Cell class.
+	 */
 	Cell();
+
+	/**
+	 * @brief Gets the mine status of the cell.
+	 * @return True if the cell contains a mine, false otherwise.
+	 */
 	bool getIsMine() const;
+
+	/**
+	 * @brief Sets the mine status of the cell.
+	 */
 	void setMine();
+
+	/**
+	 * @brief Gets the reveal status of the cell.
+	 * @return True if the cell has been revealed, false otherwise.
+	 */
 	bool getIsRevealed() const;
+
+	/**
+	 * @brief Reveals the cell.
+	 */
 	void reveal();
 };
